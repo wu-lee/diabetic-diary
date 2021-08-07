0c382f31fe7dfa06dd61bdbef785706fdff5ada3
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2021-08-07 12:37:43 +0100

    main.dart - switch to Moor database

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 
-import 'package:diabetic_diary/database/mock_database.dart';
 import 'package:flutter/widgets.dart';
 import 'app.dart';
 import 'database.dart';
+import 'database/moor_database.dart';
 
 void main() async {
-  Database db = MockDatabase();
+  Database db = MoorDatabase.create();
   Database.initialiseData(db);
   runApp(App(title: 'Diabetic Diary', db: db));
 }
