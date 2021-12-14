ad61c8093653c0f5e14b16c8ddb7b03ff2894468
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2021-12-14 22:27:53 +0000

    labels.dart - generate, and use in translation.dart
    HACK: we hardwired the language code to `en`

---
+++ lib/labels.dart
@@ -0,0 +1,184 @@
+
+const LABELS = {
+  #AvailableIngredients: {
+    #en: 'Available Ingredients',
+  },
+  #AvailableItems: {
+    #en: 'Available Items',
+  },
+  #AvailableStats: {
+    #en: 'Available Statistics',
+  },
+  #Cabbage: {
+    #en: 'Cabbage',
+  },
+  #Caffeine: {
+    #en: 'Caffeine',
+  },
+  #cal: {
+    #en: 'cal',
+  },
+  #cal_per_g: {
+    #en: 'cal/g',
+  },
+  #Carbs: {
+    #en: 'Carbohydrates',
+  },
+  #CompositionStats: {
+    #en: 'Composition Statistics',
+  },
+  #Contents: {
+    #en: 'Contents',
+  },
+  #DeleteFromIndex: {
+    #en: 'Delete from index',
+  },
+  #Dishes: {
+    #en: 'Dishes',
+  },
+  #Edit: {
+    #en: 'Edit',
+  },
+  #Energy: {
+    #en: 'Energy',
+  },
+  #EnergyByMass: {
+    #en: 'Energy by Mass',
+  },
+  #ErrorCalculatingStats: {
+    #en: 'Error calculating statistics',
+  },
+  #Fat: {
+    #en: 'Fat',
+  },
+  #Fibre: {
+    #en: 'Fibre',
+  },
+  #FractionalMass: {
+    #en: 'Fractional Mass',
+  },
+  #FractionByMass: {
+    #en: 'Fraction by Mass',
+  },
+  #g: {
+    #en: 'g',
+  },
+  #g_per_cg: {
+    #en: 'g/cg',
+  },
+  #g_per_g: {
+    #en: 'g/g',
+  },
+  #g_per_hg: {
+    #en: 'g/100g',
+  },
+  #g_per_kg: {
+    #en: 'g/kg',
+  },
+  #g_per_mg: {
+    #en: 'g/mg',
+  },
+  #HomePage: {
+    #en: 'Home Page',
+  },
+  #Ingredients: {
+    #en: 'Ingredients',
+  },
+  #Items: {
+    #en: 'Items',
+  },
+  #J: {
+    #en: 'J',
+  },
+  #J_per_g: {
+    #en: 'J/g',
+  },
+  #J_per_hg: {
+    #en: 'J/100g',
+  },
+  #J_per_kg: {
+    #en: 'J/kg',
+  },
+  #kcal: {
+    #en: 'kcal',
+  },
+  #kcal_per_g: {
+    #en: 'kcal/g',
+  },
+  #kcal_per_hg: {
+    #en: 'kcal/100g',
+  },
+  #kg: {
+    #en: 'kg',
+  },
+  #kJ: {
+    #en: 'kJ',
+  },
+  #kJ_per_g: {
+    #en: 'kJ/g',
+  },
+  #Mass: {
+    #en: 'Mass',
+  },
+  #MealId: {
+    #en: 'Meal ID',
+  },
+  #MealNotes: {
+    #en: 'Meal Notes',
+  },
+  #Meals: {
+    #en: 'Meals',
+  },
+  #MealTime: {
+    #en: 'Mile Time',
+  },
+  #mg: {
+    #en: 'mg',
+  },
+  #NewDish: {
+    #en: 'New Dish',
+  },
+  #NewIngredient: {
+    #en: 'New Ingredient',
+  },
+  #NewMeal: {
+    #en: 'New Meal',
+  },
+  #Overview: {
+    #en: 'Overview',
+  },
+  #Protein: {
+    #en: 'Protein',
+  },
+  #Salad: {
+    #en: 'Salad',
+  },
+  #Salt: {
+    #en: 'Salt',
+  },
+  #SaturatedFat: {
+    #en: 'Saturated Fat',
+  },
+  #Sodium: {
+    #en: 'Sodium',
+  },
+  #Sugar: {
+    #en: 'Sugar',
+  },
+  #Tahini: {
+    #en: 'Tahini',
+  },
+  #ug: {
+    #en: 'µg',
+  },
+  #YouHaveNDishes: {
+    #en: 'You have %{n} Dishes',
+  },
+  #YouHaveNIngredients: {
+    #en: 'You have %{n} Ingredients',
+  },
+  #YouHaveNMacroNutrients: {
+    #en: 'You have %{n} Macronutrients',
+  },
+};
+
--- lib/translation.dart
+++ lib/translation.dart
@@ -1,22 +1,20 @@
 
 
-const Translations = <Symbol, String>{
-  #YouHaveNDishes: "You have %{n} dishes",
-  #YouHaveNIngredients: "You have %{n} ingredients",
-  #YouHaveNMacroNutrients: "You have %{n} macro nutrients",
-};
-
+import 'labels.dart';
 
 /// Translate a symbol in the given context into a string in the target language
 /// (as defined elsewhere)
 String TL8(Symbol symbol, [Map<Symbol, dynamic> params = const {}]) {
-  final translation = Translations[symbol];
-  if (translation != null)
-    return translation.replaceAllMapped(RegExp(r'%\{(.+?)\}'),
-            (match) => params[Symbol('${match[1]}')]?.toString() ?? '${match[0]}');
-  // Stopgap implementation: toString returns 'Symbol("name here")', snip off the
-  // boilerplate
-  return symbolToString(symbol);
+  final translations = LABELS[symbol];
+  if (translations == null)
+    return symbolToString(symbol);
+
+  final translation = translations[#en]; // FIXME unhardwire!
+  if (translation == null)
+    return symbolToString(symbol); // Make a best effort
+
+  return translation.replaceAllMapped(RegExp(r'%\{(.+?)\}'),
+          (match) => params[Symbol('${match[1]}')]?.toString() ?? '${match[0]}');
 }
 
 String symbolToString(Symbol symbol) {
