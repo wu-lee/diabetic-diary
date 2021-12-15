110e2bec6ea05482c38ba527a1a18346ca16b2e3
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2021-12-15 11:46:38 +0000

    composite_edible.dart - remove duplicate methods

--- lib/composite_edible.dart
+++ lib/composite_edible.dart
@@ -22,47 +22,6 @@ abstract class CompositeEdible extends Edible {
               (sum, quantity) => sum + quantity.units.multiplier*quantity.amount
       );
 
-  Future<Map<Symbol, Quantity>> invalidContents(Database db, [Map<Symbol, Measurable>? cache]);
-
-  /// Checks for contents with inconsistent units.
-  ///
-  /// Specifically, this expects all contents to have contents with units of #Mass.
-  ///
-  /// If [cache] is supplied, uses this for keeping a shared cache of measurables
-  /// for a little added speed.
-  ///
-  /// Returns a list of maps of invalid content items - obviously, this list will be empty
-  /// if the contents are fully valid.
-  Future<Map<Symbol, Quantity>> invalidMeasurableContents(Database db, [Map<Symbol, Measurable>? cache]) async {
-    cache ??= {};
-    final invalids = <Symbol, Quantity>{};
-    for(final item in contents.entries) {
-      final measurable = cache[item.key] ??= await db.measurables.fetch(item.key);
-      if (item.value.units.dimensionsId != measurable.defaultUnits.dimensionsId)
-        invalids[item.key] = item.value;
-    }
-    return invalids;
-  }
-
-  /// Checks for contents with inconsistent units.
-  ///
-  /// Specifically, this expects all contents to have contents with units of #Mass.
-  ///
-  /// If [cache] is supplied, uses this for keeping a shared cache of measurables
-  /// for a little added speed.
-  ///
-  /// Returns a list of maps of invalid content items - obviously, this list will be empty
-  /// if the contents are fully valid.
-  Future<Map<Symbol, Quantity>> invalidMassContents(Database db, [Map<Symbol, Measurable>? cache]) async {
-    cache ??= {};
-    final invalids = <Symbol, Quantity>{};
-    for(final item in contents.entries) {
-      if (item.value.units.dimensionsId != Dimensions.Mass.id)
-        invalids[item.key] = item.value;
-    }
-    return invalids;
-  }
-
   /// Get the total mass of the contents in grams
   ///
   /// Assumes that the contents all have units of mass and do not overlap!
